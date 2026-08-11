af2e8a732a6a5a2dcd4d1373d654f5a77a1bdd11
Kush kumar Bharti <try.kushbharti@gmail.com>
2026-08-11 07:57:48 +0530

    Time: 3 ms (53.73%), Space: 20.6 MB (6.78%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -4,7 +4,7 @@ class Solution:
 
         for i,num in enumerate(nums):
             needed = target - num
-
+            # is present or not.
             if needed in seen:
                 return [seen[needed],i]
             seen[num] = i
